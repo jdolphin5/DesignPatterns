e3806a49603e00236b43fcb1607cc385b69d0e85
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2024-09-06 16:56:32 +1000

    init

---
+++ adapter/ducks/Duck.java
@@ -0,0 +1,7 @@
+package adapter.ducks;
+
+public interface Duck {
+	public void quack();
+
+	public void fly();
+}
---
+++ adapter/ducks/Turkey.java
@@ -0,0 +1,7 @@
+package adapter.ducks;
+
+public interface Turkey {
+	public void gobble();
+
+	public void fly();
+}
